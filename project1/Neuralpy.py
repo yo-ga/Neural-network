7352ab4e6c81ec5e354f8f098593cf234b020278
YOGA <art96962006@gmail.com>
2016-10-11 16:08:41 +0800

    Make a simple two-mode menu.

--- project1/Neuralpy.py
+++ project1/Neuralpy.py
@@ -7,16 +7,23 @@ import sys
 
 def parse():
 	parser = ArgumentParser(description='Neutral net analysis tool.')
-	parser.add_argument('-o', '--output-prefix', help='Set the output file prefix', action='store', nargs='?', dest='outPre', default='Neutru'+str(date.today()))
+	parser.add_argument('-o', '--output-prefix', help='Set the output file prefix', action='store', dest='outPre', default='Neutru'+str(date.today()))
 	parser.add_argument('-O', '--output-destination', help='Set the output file directory', action='store',dest='outDir',default='./')
-	parser.add_argument('-T', '--testing-data', help='Set the file to test the program, including training data and examinng data', dest='testData',type=argparse.FileType('r'))
-	parser.add_argument('-t', '--training-data', help='Set the file for training data', action='store', dest='trainData', type=argparse.FileType('r'))
-	parser.add_argument('-e', '--examining-data', help='Set the file to examine the data', action='store', dest='examData', type=argparse.FileType('r'))
+	parser.add_argument('-T', '--test-Data', help='Set the file to test the program, including training data and examinng data', dest='testData',type=argparse.FileType(mode='r',encoding='utf-8'))
+	parser.add_argument('-t', '--train-data', help='Set the file for training data', action='store', dest='trainData', type=argparse.FileType(mode='r',encoding='utf-8'))
+	parser.add_argument('-e', '--exam-data', help='Set the file to examine the data', action='store', dest='examData', type=argparse.FileType(mode='r',encoding='UTF-8'))
 	#parser.add_argument('-v', '--verbose', help='Increase output verbosity', action='store_true', dest='verbose')
 	args = parser.parse_args()
+	return args
 
 def main():
-	parse()
+	args=parse()
+	if((args.testData is not None or args.trainData is not None )and args.examData is not None):
+		print ("Error: you couldn\'t use two modes in the same time.")
+	elif (args.testData is not None):
+
+	elif (args.trainData is not None and args.examData is not None):
+
 
 if __name__ == "__main__":
 	main()
